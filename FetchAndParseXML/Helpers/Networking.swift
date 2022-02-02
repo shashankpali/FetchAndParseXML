918edf88ba09b43c2a90e6303c4ffb0d63453ad8
Shashank Pali <shashank.pali@aventri.com>
2022-02-02 19:07:11 +0530

    ADDED - network class for fetching proess

---
+++ FetchAndParseXML/Helpers/Networking.swift
@@ -0,0 +1,26 @@
+//
+//  Networking.swift
+//  FetchAndParseXML
+//
+//  Created by Shashank Pali on 02/02/22.
+//
+
+import Foundation
+
+struct Networking {
+    
+    static func get(forURLString: String, callback: @escaping (Bool, Data?, String?) -> Void) {
+
+        var request = URLRequest(url: URL(string: forURLString)!,timeoutInterval: 30)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let res = data else {
+                callback(false, nil, String(describing: error))
+                return
+            }
+            callback(true, res, nil)
+        }.resume()
+        
+    }
+}
