4e77296f0db1a13c6280bf7dcbe60abc561dc09c
Shashank Pali <shashank.pali@aventri.com>
2022-02-02 18:32:06 +0530

    ADDED - cell to show parse data

---
+++ FetchAndParseXML/View/LoadXMLCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadXMLCell.swift
+//  FetchAndParseXML
+//
+//  Created by Shashank Pali on 02/02/22.
+//
+
+import UIKit
+
+class LoadXMLCell: UITableViewCell {
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
