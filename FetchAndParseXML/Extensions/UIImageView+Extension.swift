f6302eb9b49a7eda0dba63365cf43f0cf0959e1b
Shashank Pali <shashank.pali@aventri.com>
2022-02-02 23:39:22 +0530

    ADDED - async image loader

---
+++ FetchAndParseXML/Extensions/UIImageView+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIImageView+Extension.swift
+//  FetchAndParseXML
+//
+//  Created by Shashank Pali on 02/02/22.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    public func imageFromServerURL(urlString: String, PlaceHolderImage:UIImage) {
+        
+        if self.image == nil{
+            self.image = PlaceHolderImage
+        }
+        
+        let loader = UIActivityIndicatorView.init(style: .large)
+        self.addSubview(loader)
+        loader.translatesAutoresizingMaskIntoConstraints = false
+        loader.make(center: .horizontally, toView: self)
+        loader.make(center: .vertically, toView: self)
+        loader.set(widthRatio: 0.8, respectToView: self)
+        loader.startAnimating()
+        
+        Networking.get(forURLString: urlString) { success, data, errorString in
+            DispatchQueue.main.async(execute: { () -> Void in
+                loader.removeFromSuperview()
+                self.image = UIImage.init(named: "BlankImgHolder")!
+                guard success else {return}
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+        }
+    }}
--- FetchAndParseXML/Extensions/UIView+Extension.swift
+++ FetchAndParseXML/Extensions/UIView+Extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIView {
+    
     enum Center {
     case horizontally
     case vertically
